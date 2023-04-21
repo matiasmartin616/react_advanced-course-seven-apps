1c541628ef2e473b6693dce7feae73959fa4358d
mmartinamezcua <mmartinamezcua@hiberus.com>
2023-04-21 14:05:47 +0200

    Add standard lint

--- project/01 Twitter Follow Card/src/App.jsx
+++ project/01 Twitter Follow Card/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import "./App.css"
-import { TwitterFollowCard } from "./TwitterFollowCard.jsx"
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App() {
   const users = [
