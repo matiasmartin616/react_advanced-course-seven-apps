84b6f40594162421528afe1373fdae7c86de25a3
mmartinamezcua <mmartinamezcua@hiberus.com>
2023-04-21 16:51:10 +0200

    03 project

---
+++ project/03 mouse follower/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import { FollowMouse } from './components/FollowMouse'
+
+function App () {
+  const [mounted, setMounted] = useState(true)
+
+  return (
+    <main>
+      {mounted && <FollowMouse />}
+      <button onClick={() => setMounted(!mounted)}>
+        Toggle mounted FollowMouse component
+      </button>
+    </main>
+  )
+}
+
+export default App
